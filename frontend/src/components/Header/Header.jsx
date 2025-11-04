36580a899def31b53087ec123c990aa6d3683b27
bryancalvarenga <bryancalvarenga@outlook.com.br>
2025-11-04 16:02:36 -0300

    first commit: header, home & explore-menu

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit rem adipisci veritatis quis sint inventore! Tempore nobis quidem, autem perferendis nesciunt omnis magni quam temporibus debitis quae ut fugiat fuga.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
