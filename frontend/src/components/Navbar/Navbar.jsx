63d93c792922664a6fae394a66cf7c865afdccce
bryancalvarenga <bryancalvarenga@outlook.com.br>
2025-11-06 21:13:39 -0300

    Refactor Navbar and Cart components; add total cart amount calculation and enhance cart layout

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,53 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import assets from '../../assets/assets';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Navbar.css";
+import assets from "../../assets/assets";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import { useContext } from "react";
 
-const Navbar = ({setShowLogin}) => {
-
+const Navbar = ({ setShowLogin }) => {
   // Estado para controlar qual item do menu está selecionado
   const [menu, setMenu] = useState("home");
 
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
-    <div className='navbar'>
-      
+    <div className="navbar">
       {/* Logo da marca */}
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
 
       {/* Menu de navegação */}
       <ul className="navbar-menu">
-
         {/* Cada item altera o estado para mostrar qual está ativo */}
-        <Link 
-          to='/'
-          onClick={() => setMenu("home")} 
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           home
         </Link>
 
-        <a href='#explore-menu' 
-          onClick={() => setMenu("menu")} 
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           menu
         </a>
 
-        <a href='#app-download'  
-          onClick={() => setMenu("mobile-app")} 
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
           className={menu === "mobile-app" ? "active" : ""}
         >
           mobile-app
         </a>
 
-        <a href='#footer'  
-          onClick={() => setMenu("contact-us")} 
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
           className={menu === "contact-us" ? "active" : ""}
         >
           contact us
@@ -50,18 +56,19 @@ const Navbar = ({setShowLogin}) => {
 
       {/* Ícones e botão do lado direito */}
       <div className="navbar-right">
-
         {/* Ícone de busca */}
         <img src={assets.search_icon} alt="" />
 
         {/* Ícone do carrinho com indicador (dot) */}
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
 
         {/* Botão para login */}
-        <button onClick={()=>setShowLogin(true)}>Sign in</button>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   );
